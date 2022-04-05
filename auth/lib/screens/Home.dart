769b4e50634741ee2cc51fd8f89805e6dcb6e696
joelledaou <joelle.daou@net.usj.edu.lb>
2022-04-05 13:25:14 +0300

    Authentication - First commit

---
+++ auth/lib/screens/Home.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'SignIn.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: ElevatedButton(
+      child: Text("Logout"),
+      onPressed: () {
+        FirebaseAuth.instance.signOut().then((value) {
+          print("Signed out");
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => SignIn()));
+        });
+      },
+    )));
+  }
+}
---
+++ auth/lib/screens/SignIn.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../utils/colors.dart';
+import '../reusable_widgets/reusable_widgets.dart';
+import 'Home.dart';
+import 'SignUp.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+          hexStringToColor("CB2B93"),
+          hexStringToColor("9546C4"),
+          hexStringToColor("5E61F4")
+        ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget("assets/images/logo1.png"),
+                SizedBox(height: 45),
+                reusableTextField("Enter username", Icons.person_outline, false,
+                    _emailTextController),
+                SizedBox(height: 30),
+                reusableTextField("Enter password", Icons.lock_outline, true,
+                    _passwordTextController),
+                SizedBox(height: 20),
+                SignInSignUpButton(context, true, () {
+                  FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: _emailTextController.text,
+                      password: _passwordTextController.text)
+                      .then((value) {
+                        print("Login successful");
+                        Navigator.push(
+                          context, MaterialPageRoute(builder: (context) => Home()));
+                      })
+                      .onError((error, stackTrace) {
+                        print("Error ${error.toString()}");
+                      });
+                }),
+                signUpOption()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row signUpOption() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Don't have an account? Register",
+            style: TextStyle(color: Colors.white70)),
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SignUp()));
+          },
+          child: const Text(
+            " here",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
